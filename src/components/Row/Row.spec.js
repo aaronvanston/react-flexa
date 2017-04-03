31c651ae5539afc5694e9474ca2d3b02ff89ed7b
Aaron Vanston <aaron.vanston@rea-group.com>
2017-04-03 18:53:02 +1000

    chore: add root import support for test directory

---
+++ src/components/Row/Row.spec.js
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe('<Row />', () => {
+  it('Should ...', () => {
+    expect(true).to.be.true();
+  });
+});
---
+++ test/chai.js
@@ -0,0 +1,6 @@
+import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+
+chai.use(chaiEnzyme());
+
+export default chai;
