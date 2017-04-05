ccc858fca7ac4beddab8952bf2a4243a2ca3d7e0
Aaron Vanston <aaron.vanston@rea-group.com>
2017-04-05 21:46:09 +1000

    feat: add prop vlaidation for COl

---
+++ src/components/Col/Col.js
@@ -0,0 +1,41 @@
+import { PropTypes } from 'react';
+
+const Col = '';
+
+const alignSelfOptions = ['auto', 'flex-start', 'flex-end', 'center', 'baseline', 'stretch'];
+
+Col.PropTypes = {
+  xs: PropTypes.number,
+  sm: PropTypes.number,
+  md: PropTypes.number,
+  lg: PropTypes.number,
+  hidden: PropTypes.bool,
+
+  gutter: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+    PropTypes.bool,
+  ]),
+
+  order: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.shape({
+      xs: PropTypes.number,
+      sm: PropTypes.number,
+      md: PropTypes.number,
+      lg: PropTypes.number,
+    }),
+  ]),
+
+  alignSelf: PropTypes.oneOfType([
+    PropTypes.oneOf(alignSelfOptions),
+    PropTypes.shape({
+      xs: PropTypes.oneOf(alignSelfOptions),
+      sm: PropTypes.oneOf(alignSelfOptions),
+      md: PropTypes.oneOf(alignSelfOptions),
+      lg: PropTypes.oneOf(alignSelfOptions),
+    }),
+  ]),
+};
+
+export default Col;
