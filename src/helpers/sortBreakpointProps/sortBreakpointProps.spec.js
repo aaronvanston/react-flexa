4f54c0224f60ff87f988c93de6d540534d855053
Aaron Vanston <aaron.vanston@rea-group.com>
2017-04-17 16:02:38 +1000

    refactor: tests from mocha to jest

--- src/helpers/sortBreakpointProps/sortBreakpointProps.spec.js
+++ src/helpers/sortBreakpointProps/sortBreakpointProps.spec.js
@@ -1,5 +1,3 @@
-import chai from '@/chai';
-
 import { themeProvider } from '~/theme';
 
 import sortBreakpointProps from './sortBreakpointProps';
@@ -9,11 +7,11 @@ const { breakpoints } = themeProvider;
 describe('sortBreakpointProps', () => {
   it('should remove props that are not a valid breakpoint', () => {
     const mockProps = { xs: 1, sm: 2, md: 3, lg: 4, xlg: 5 };
-    chai.expect(sortBreakpointProps(mockProps)).to.eql(breakpoints);
+    expect(sortBreakpointProps(mockProps)).toEqual(breakpoints);
   });
 
   it('should order breakpoint props from smallest to largest value', () => {
     const mockProps = { lg: 5, sm: 2, xs: 3, md: 4 };
-    chai.expect(sortBreakpointProps(mockProps)).to.eql(['xs', 'sm', 'md', 'lg']);
+    expect(sortBreakpointProps(mockProps)).toEqual(['xs', 'sm', 'md', 'lg']);
   });
 });
