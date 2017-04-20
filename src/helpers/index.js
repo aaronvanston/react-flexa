8d6a22d29f5138a01493b51a0a1b06fe7d827a8a
Aaron Vanston <aaron.vanston@rea-group.com>
2017-04-20 21:55:49 +1000

    feat: add sortBreakpoints helper func

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,4 @@
 export columnWidth from './columnWidth/columnWidth';
 export * as gutter from './gutter/gutter';
 export mediaQuery from './mediaQuery/mediaQuery';
+export sortBreakpoints from './sortBreakpoints/sortBreakpoints';
