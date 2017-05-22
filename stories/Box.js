b949a4a588a51c848505da25541b827aa8593ffc
Angus Fretwell <angusfretwell@me.com>
2017-05-22 13:41:57 +1000

    refactor: split examples across multiple files

---
+++ stories/Box.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  height: 1rem;
+  padding: 1rem;
+  margin-bottom: 1rem;
+  font-family: "Avenir Next", sans-serif;
+  font-weight: 500;
+  line-height: 1rem;
+  text-align: center;
+  color: white;
+  background-color: dodgerblue;
+`;
--- stories/index.js
+++ stories/index.js
@@ -1,35 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 import { storiesOf } from '@storybook/react';
 
-import { Row, Col } from '../src';
-
-const Box = styled.div`
-  background-color: DodgerBlue;
-  color: white;
-  font-family: "Avenir Next", sans-serif;
-  font-weight: 500;
-  padding: 1rem;
-  text-align: center;
-  margin-bottom: 1rem;
-  height: 1rem;
-  line-height: 1rem;
-`;
-
-storiesOf('Responsive', module)
-  .add('Basic', () => (
-    <Row flexWrap="wrap">
-      <Col xs={12} sm={3} md={2} lg={1}><Box /></Col>
-      <Col xs={6} sm={6} md={8} lg={10}><Box /></Col>
-      <Col xs={6} sm={3} md={2} lg={1}><Box /></Col>
-    </Row>
-  ));
+import Fluid from './Fluid';
+import Responsive from './Responsive';
 
 storiesOf('Fluid', module)
-  .add('Basic', () => (
-    <Row flexWrap="wrap">
-      <Col xs={12}><Box /></Col>
-      <Col xs={6}><Box /></Col>
-      <Col xs={6}><Box /></Col>
-    </Row>
-  ));
+  .add('Basic', () => <Fluid />);
+
+storiesOf('Responsive', module)
+  .add('Basic', () => <Responsive />);
