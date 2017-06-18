d96f3b22ecb337294e88b6dee9629ceb7aae874c
Aaron Vanston <aaron.vanston@rea-group.com>
2017-06-18 22:21:24 +1000

    chore: add withreadme to storybook

--- src/components/Col/Col.stories.js
+++ src/components/Col/Col.stories.js
@@ -3,11 +3,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import withReadme from 'storybook-readme/with-readme';
 
 import { Col, Fluid, Responsive } from './stories';
+import README from './README.md';
 
 storiesOf('Col', module)
   .addDecorator(withKnobs)
+  .addDecorator(withReadme(README))
   .add('Default', () => <Col />)
   .add('Fluid example', () => <Fluid />)
   .add('Responsive example', () => <Responsive />);
