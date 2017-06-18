5a452d99dab0e63897a0b9d3a425f4fcb2701ea1
Angus Fretwell <angusfretwell@me.com>
2017-06-18 22:35:53 +1000

    refactor: gutter width function

--- src/helpers/gutter/gutter.js
+++ src/helpers/gutter/gutter.js
@@ -9,25 +9,17 @@ import { themeProvider } from '../../theme';
 const { theme } = themeProvider;
 
 export const gutterWidth = (props, breakpoint) => {
-  // Does the gutter prop exists?
   if (has(props, 'gutter')) {
-    // is the gutter a responsive object?
-    if (isObject(props.gutter)) {
-      // does the object actually include the current mapped breakpoint?
-      if (has(props, `gutter.${breakpoint}`)) {
-        // return the value within the gutter object
-        return props.gutter[breakpoint];
-      }
-      // if prop does not exist in gutter object use theme default
-      // TODO make function for getting theme default
-      return isObject(theme(props).gutter) ? theme(props).gutter[breakpoint] : theme(props).gutter;
+    if (isObject(props.gutter) && has(props, `gutter.${breakpoint}`)) {
+      return props.gutter[breakpoint];
     }
+    
     return props.gutter;
   }
 
-  // If the prop does not exists use the theme provider
-  // TODO make function for getting theme default
-  return isObject(theme(props).gutter) ? theme(props).gutter[breakpoint] : theme(props).gutter;
+  return isObject(theme(props).gutter) 
+    ? theme(props).gutter[breakpoint] 
+    : theme(props).gutter;
 };
 
 export const row = (props, breakpoint) => {
