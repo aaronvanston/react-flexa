37f4378ec34d14334b05478ca06929d82d3317b7
Aaron Vanston <aaron.vanston@rea-group.com>
2017-07-01 20:42:56 +1000

    refactor: media query to accept custom width

--- src/helpers/mediaQuery/mediaQuery.js
+++ src/helpers/mediaQuery/mediaQuery.js
@@ -1,21 +1,10 @@
 import { css } from 'styled-components';
 
-import { themeProvider } from '../../theme';
-
-const { theme } = themeProvider;
-
-const mediaQuery = Object.keys(theme().breakpoints).reduce((accumulator, value) => {
-  const breakpointSize = theme().breakpoints[value];
-
-  Object.assign(accumulator, { [value]: (...args) =>
-    (breakpointSize === 0 ? css`${css(...args)}` :
-      css`@media (min-width: ${breakpointSize}rem) {
-        ${css(...args)}
-      }`
-    ),
-  });
-
-  return accumulator;
-}, {});
+const mediaQuery = (width, styles) => (
+  (width === 0) ? css`${styles.split(',').join('')}` :
+  css`@media (min-width: ${width}rem) {
+    ${styles.split(',').join('')}
+  }`
+);
 
 export default mediaQuery;
